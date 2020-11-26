ad3f94caa6fed3347c353ea1666809fd2a1009e3
S <hipdud38@gmail.com>
2020-11-26 01:19:37 +0000

    extract methods

--- src/main/BusTripMapper.java
+++ src/main/BusTripMapper.java
@@ -3,13 +3,6 @@ package main;
 import main.model.Tap;
 import main.model.Trip;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,50 +19,66 @@ public class BusTripMapper {
 
     /**
      * Map the credit card payment taps to a list of bus trips.
+     * @param taps
      * @return the list of bus trips
      */
     public List<Trip> mapTrips(List<Tap> taps) {
         taps.stream().filter(t -> t.getTapType().equals("ON")).forEach(firstStop -> {
-            Tap secondStop;
-
-            // Get the next off tap with same pan
-            List<Tap> samePan = taps.stream()
-                    .filter(t -> t.getPan().equals(firstStop.getPan()))
-                    .filter(t -> t.getCompanyId().equals(firstStop.getCompanyId()))
-                    .filter(t -> t.getBusId().equals(firstStop.getBusId()))
-                    .filter(t -> t.getDateTime().after(firstStop.getDateTime()))
-                    .filter(t -> t.getTapType().equals("OFF"))
-                    .collect(Collectors.toList());
-            if (samePan.size() > 0) {
-                secondStop = samePan.get(0);
-            } else {
-                secondStop = null;
-            }
-
-            Trip trip = new Trip();
-            trip.setStarted(firstStop.getDateTime());
-            if (secondStop != null) {
-                trip.setFinished(secondStop.getDateTime());
-                trip.setToStopId(secondStop.getStopId());
-                if (firstStop.getStopId().equals(secondStop.getStopId())) {
-                    trip.setStatus("CANCELLED");
-                } else {
-                    trip.setStatus("COMPLETE");
-                }
-
-            } else {
-                trip.setFinished(null);
-                trip.setToStopId(null);
-                trip.setStatus("INCOMPLETE");
-            }
-            trip.setFromStopId(firstStop.getStopId());
-            trip.setCompanyId(firstStop.getCompanyId());
-            trip.setBusId(firstStop.getBusId());
-            trip.setPan(firstStop.getPan());
-            trip.setChargeAmount(Utility.calculateCharge(trip.getFromStopId(), trip.getToStopId()));
-
-            trips.add(trip);
+            Tap secondStop = getSecondTap(firstStop, taps);
+            trips.add(createTrip(firstStop, secondStop));
         });
         return trips;
+    }
+
+    /**
+     * Get the second stop given the first stop and the list of taps
+     * @param firstStop
+     * @param taps
+     * @return
+     */
+    public Tap getSecondTap(Tap firstStop, List<Tap> taps) {
+        List<Tap> samePan = taps.stream()
+                .filter(t -> t.getPan().equals(firstStop.getPan()))
+                .filter(t -> t.getCompanyId().equals(firstStop.getCompanyId()))
+                .filter(t -> t.getBusId().equals(firstStop.getBusId()))
+                .filter(t -> t.getDateTime().after(firstStop.getDateTime()))
+                .filter(t -> t.getTapType().equals("OFF"))
+                .collect(Collectors.toList());
+        if (samePan.size() > 0) {
+            return samePan.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Create a trip from the first stop and second stop
+     * @param firstStop
+     * @param secondStop
+     * @return
+     */
+    public Trip createTrip(Tap firstStop, Tap secondStop) {
+        Trip trip = new Trip();
+        trip.setStarted(firstStop.getDateTime());
+        if (secondStop != null) {
+            trip.setFinished(secondStop.getDateTime());
+            trip.setToStopId(secondStop.getStopId());
+            if (firstStop.getStopId().equals(secondStop.getStopId())) {
+                trip.setStatus("CANCELLED");
+            } else {
+                trip.setStatus("COMPLETE");
+            }
+
+        } else {
+            trip.setFinished(null);
+            trip.setToStopId(null);
+            trip.setStatus("INCOMPLETE");
+        }
+        trip.setFromStopId(firstStop.getStopId());
+        trip.setCompanyId(firstStop.getCompanyId());
+        trip.setBusId(firstStop.getBusId());
+        trip.setPan(firstStop.getPan());
+        trip.setChargeAmount(Utility.calculateCharge(trip.getFromStopId(), trip.getToStopId()));
+        return trip;
     }
 }
